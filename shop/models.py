ceb4a3a45432c6e340a9acd8f3ab5ea9c1ecbd01
vladimir999999 <vova03022011@gmail.com>
2026-04-06 20:31:01 +0300

    06.04.26

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="images/Product/")
+    desc = models.TextField()
+    price = models.IntegerField()
+    discount = models.PositiveIntegerField(default=0)
+    rating = models.PositiveIntegerField()
+    stock = models.PositiveIntegerField()
+    is_available = models.BooleanField(default=True)
